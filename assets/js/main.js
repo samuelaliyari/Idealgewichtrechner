0f6689606d0ad13b62f4036b41fd451b7e772785
Samuel Aliyari <samuelaliyari@outlook.de>
2023-10-30 13:05:21 +0100

    v1.0.0

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+
+
+const heightInput = document.body.querySelector("#bodyHeight");
+const ageInput = document.body.querySelector("#age");
+const typeBreit = document.body.querySelector("#breit");
+const typeSchmal = document.body.querySelector("#schmal");
+const resultOutput = document.body.querySelector("p");
+const submitButton = document.body.querySelector("div input");
+
+submitButton.addEventListener("click", () => {
+    event.preventDefault();
+    const height = Number(heightInput.value);
+    const age = Number(ageInput.value);
+    const breit = typeBreit.checked;
+    const schmal = typeSchmal.checked;
+    
+
+    if (schmal) {
+        
+        resultOutput.textContent = `Dein Idealgewicht ist ${parseFloat(((height - 100) + (age/ 10)) * 0.9 * 0.9).toFixed(1)} kg.`;
+    }
+    if (breit) {
+        resultOutput.textContent = `Dein Idealgewicht ist ${parseFloat(((height - 100) + (age/ 10)) * 0.9 * 1.1).toFixed(1)} kg.`;
+
+    }
+})
+
+
